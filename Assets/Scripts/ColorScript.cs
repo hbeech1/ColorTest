7b88a4148473daba322791dbc00c0692930d7f3a
hbeech1 <55199696+hbeech1@users.noreply.github.com>
2019-09-20 13:45:19 -0400

    Assets for the test.
    Controls
    ---------
    W: White
    B: Black
    R: Red
    G: Green
    Y: Yellow
    C: Cyan
    M: Magenta

---
+++ Assets/Scripts/ColorScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    int track; //track keeps track of the times the color has been changed in the log.
+    bool change; // change is true if color changed in its frame, otherwise is false.
+    void Start()
+    {
+        track = 0;
+        change = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = false;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+            change = true;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+            change = true;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            change = true;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+            change = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+            change = true;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+            change = true;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+            change = true;
+        }
+        if (change)
+        {
+            track += 1;
+            Debug.Log(track);
+        }
+    }
+}
